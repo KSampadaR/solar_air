d12cdf79182f462910fed874409de21f2d8c74cd
Sampada  Khopade <sampada.khopade22@gmail.com>
2024-10-10 20:14:56 +0530

    change

--- machine-learning/personalized_weather_alerts.py
+++ machine-learning/personalized_weather_alerts.py
@@ -2,12 +2,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
 def train_model(weather_data, user_preferences):
-    """
-    Trains a machine learning model to predict personalized weather alerts based on user preferences and weather data.
-    :param weather_data: DataFrame with weather data
-    :param user_preferences: DataFrame with user preferences data
-    :return: trained model
-    """
+    
     # Merge datasets and prepare them for training
     data = pd.merge(weather_data, user_preferences, on='user_id')
     features = data.drop('alert_needed', axis=1)
@@ -19,13 +14,7 @@ def train_model(weather_data, user_preferences):
     return model
 
 def predict(model, weather_data, user_preferences):
-    """
-    Predicts personalized weather alerts based on user preferences and weather data.
-    :param model: trained model
-    :param weather_data: DataFrame with weather data
-    :param user_preferences: DataFrame with user preferences data
-    :return: DataFrame with predictions
-    """
+    
     # Merge datasets and prepare them for prediction
     data = pd.merge(weather_data, user_preferences, on='user_id')
     features = data.drop('alert_needed', axis=1)
